910d1b3237393151938b5072a86f6e0fad9bb67d
guptaprakash29 <prakashgupta8521@gmail.com>
2025-10-16 14:44:10 +0530

    Third Part Complete

--- bat-ball-stump.js
+++ bat-ball-stump.js
@@ -1,3 +1,11 @@
+let score = {
+  win: 0,
+  lost: 0,
+  tie: 0,
+  displayScore: function () {
+    return ` Won:${score.win}, Lost:${score.lost}, Tie: ${score.tie}`;
+  },
+};
 function generateComputerChoice() {
   let randomNumber = Math.random() * 3;
   if (randomNumber > 0 && randomNumber <= 1) {
@@ -12,33 +20,44 @@ function generateComputerChoice() {
 function getResult(userMove, computerMove) {
   if (userMove === "BAT") {
     if (computerMove === "BAT") {
+      score.tie++;
       return `It's a Tie.`;
     } else if (computerMove === "BALL") {
+      score.win++;
       return `User Won!`;
     } else {
+      score.lost++;
       return `Computer Won!`;
     }
   } else if (userMove === "BALL") {
     if (computerMove === "BAT") {
+      score.lost++;
       return `Computer Won!`;
     } else if (computerMove === "BALL") {
+      score.tie++;
       return `It's a Tie.`;
     } else {
+      score.win++;
       return `User Won!`;
     }
   } else {
     if (computerMove === "BAT") {
+      score.win++;
       return `User Won!`;
     } else if (computerMove === "BALL") {
+      score.lost++;
       return `Computer Won!`;
     } else {
+      score.tie++;
       return `It's a Tie.`;
     }
   }
 }
 
 function showResult(userMove, computerMove, result) {
+  // console.log(score);
   alert(
-    `You have choosen ${userMove} . Computer choise is ${computerMove}. ${result} `
+    `You have choosen ${userMove} . Computer choise is ${computerMove}. ${result}  
+    ${score.displayScore()}`
   );
 }
